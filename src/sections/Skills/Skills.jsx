ed7af4c92fc1553630d3f95f6e50d7e5d0381b44
Aayushkumar Yadav <140250471+Aayushkrishnaut@users.noreply.github.com>
2024-10-03 15:36:45 +0530

    Update Skills.jsx

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -3,9 +3,11 @@ import checkMarkIconDark from '../../assets/checkmark-dark.svg';
 import checkMarkIconLight from '../../assets/checkmark-light.svg';
 import SkillList from '../../common/SkillList';
 import { useTheme } from '../../common/ThemeContext';
+
 function Skills() {
   const { theme } = useTheme();
   const checkMarkIcon = theme === 'light' ? checkMarkIconLight : checkMarkIconDark;
+
   return (
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
@@ -13,25 +15,22 @@ function Skills() {
         <SkillList src={checkMarkIcon} skill="HTML" />
         <SkillList src={checkMarkIcon} skill="CSS" />
         <SkillList src={checkMarkIcon} skill="JavaScript" />
-        <SkillList src={checkMarkIcon} skill="Node.js" />
+        <SkillList src={checkMarkIcon} skill="Node" />
+        <SkillList src={checkMarkIcon} skill="Express" />
       </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="React.js" />
-        <SkillList src={checkMarkIcon} skill="Next.js" /> 
-{/*         <SkillList src={checkMarkIcon} skill="Tailwind CSS" /> */}
+        <SkillList src={checkMarkIcon} skill="React" />
+        <SkillList src={checkMarkIcon} skill="Nextjs" />
+        <SkillList src={checkMarkIcon} skill="SQL" />
       </div>
       <hr />
       <div className={styles.skillList}>
-{/*         <SkillList src={checkMarkIcon} skill="Redux" />  */}
-{/*         <SkillList src={checkMarkIcon} skill="Webpack" />  */}
         <SkillList src={checkMarkIcon} skill="Git" />
-{/*         <SkillList src={checkMarkIcon} skill="Jest" />  */}
-         <SkillList src={checkMarkIcon} skill="Tailwind" />  
+        <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
       </div>
     </section>
   );
 }
+
 export default Skills;
-
-
